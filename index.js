7cff46597599b99adf8db31ed4ff4697cab0a217
Dmeowmixer <ransonikuo@gmail.com>
2019-02-11 19:50:28 -1000

    rewriting utilizing request promise, redo index.js installs npm modules, gets DOM in Node

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
-const express = require('express');
-const fs = require('fs');
-const request = require('request');
-const cheerio = require('cheerio');
-var app = express();
+const rp = require('request-promise');
+const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 
-app.get('/scrape', function(req, res) {
-// URL TO SCRAPE
-  url = 'http://www.imdb.com/title/tt1229340/';
-  request(url, function(err,res, html){
-    console.log('hello2');
-    if(!err){
-      const $ = cheerio.load(html);
-      var title,release,rating;
-      var json = {title: "", release: "", rating: ""}
-      $('.header').filter(function(){
-        var data = $(this);
-        title = data.children().first().text();
-        json.title = title;
-        json.release = release;
-      })
-
-      $('.star-box-giga-star').filter(function(){
-        var data = $(this);
-        rating = data.text();
-        json.rating = rating;
-      })
-    }
-    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-      console.log('File written');
-    })
-    res.send('Check console');
+rp(url)
+  .then(function(html){
+    console.log(html);
   })
-});
-
-app.listen('8080');
-
-exports = module.exports = app;
+  .catch(function(err){
+    console.log(err);
+  })
